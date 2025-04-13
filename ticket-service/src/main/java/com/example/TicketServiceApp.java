81b640a65ca0dd2a1ca39cfeccca4ed22c56ff57
tesinitsyn <sitiev174@gmail.com>
2025-04-13 19:42:33 +0300

    Initial commit

---
+++ ticket-service/src/main/java/com/example/TicketServiceApp.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketServiceApp {
+    public static void main(String[] args) {
+        SpringApplication.run(TicketServiceApp.class, args);
+    }
+}
